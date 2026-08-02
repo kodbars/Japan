eb1ef9b03cf379bbbe5af92f7e5a5529893c8049
Andrey Barsukov <andrey.barsukov.bp@gmail.com>
2026-08-02 19:50:32 +0300

    7 commit

--- Japan_Server/wwwroot/js/operation.js
+++ Japan_Server/wwwroot/js/operation.js
@@ -4,3 +4,19 @@
         element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
 };
+
+window.scrollToTopInterop = {
+    init: function (dotNetHelper) {
+        // Порог появления — высота шапки (например, 150px)
+        const threshold = 150; // можно изменить под свою шапку
+
+        window.addEventListener('scroll', function () {
+            const scrollY = window.scrollY || window.pageYOffset;
+            const isVisible = scrollY > threshold;
+            dotNetHelper.invokeMethodAsync('OnScroll', isVisible);
+        });
+    },
+    scrollToTop: function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+};
